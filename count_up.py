2c0147e2a4e9f26556fe04634858fbebbf0f40b3
Parnian Jabbari <pjabb001@parrot.ib.hpcc.ucr.edu>
2020-11-06 09:27:22 -0800

    some changes

--- count_up.py
+++ count_up.py
@@ -43,17 +43,17 @@ with gzip.open(gff,"rt") as fh:
 
 # part 2 and 3 of q2
 
-	if row[2] == ("gene"): 
-		gen_count += 1
-		tot_gen_len += (int(row[4] - int(row[3])
+	if row[2] == "gene" : 
+	    gen_count += 1
+	    tot_gen_len += (int(row[4] - int(row[3])
 
 # part 5 of q2, finding rows whose second columns is CDS:
 
-	if row[2] == ("CDS"):
-		cds_count += 1
-		tot_cds_len += (int(row[4] - int(row[3]))
+	if row[2] == ("CDS") :
+	    cds_count += 1
+	    tot_cds_len += (int(row[4] - int(row[3]))
 
-print("total gene number is {}.".format(gen_count))
-printot_gen_len)
-
+print(gen_count)
+print(tot_gen_len)
+print(tot_cds_len)
 
